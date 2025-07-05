b7e173dcf44d6b932cf4abe84e78cb945dd74cfa
sokpanharith <sokpanharith.dev@gmail.com>
2025-07-05 20:16:17 +0700

    test

---
+++ src/main/java/com/cicd/beginner/TestController.java
@@ -0,0 +1,10 @@
+package com.cicd.beginner;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/test")
+@RequiredArgsConstructor
+public class TestController {}
