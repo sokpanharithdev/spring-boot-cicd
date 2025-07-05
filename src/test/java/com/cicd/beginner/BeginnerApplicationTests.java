8c14a21a9350d5bf1ab270550aed57ab7d219c80
sokpanharith <sokpanharith.dev@gmail.com>
2025-07-05 19:23:12 +0700

    Init CICD spring boot project

---
+++ src/test/java/com/cicd/beginner/BeginnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cicd.beginner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeginnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
