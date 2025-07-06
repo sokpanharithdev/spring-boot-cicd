0d73031c1111679176f97455cd254d42c6f809af
sokpanharith <sokpanharith.dev@gmail.com>
2025-07-07 00:14:38 +0700

    Config logs

--- src/main/java/com/cicd/beginner/TestController.java
+++ src/main/java/com/cicd/beginner/TestController.java
@@ -1,6 +1,7 @@
 package com.cicd.beginner;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,9 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/test")
 @RequiredArgsConstructor
+@Slf4j
 public class TestController {
   @GetMapping
   public ResponseEntity<String> test() {
+    log.info("Start calling test API...");
     return ResponseEntity.ok("Hello World");
   }
 }
