149c3056027297b35ec901253be2752cd540166d
sokpanharith <sokpanharith.dev@gmail.com>
2025-07-09 22:43:24 +0700

    test

--- src/main/java/com/cicd/beginner/consumer/UsersListenerConfig.java
+++ src/main/java/com/cicd/beginner/consumer/UsersListenerConfig.java
@@ -6,9 +6,6 @@ import io.debezium.engine.ChangeEvent;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.Message;
-
-import java.util.function.Consumer;
 
 @Configuration
 @Slf4j
