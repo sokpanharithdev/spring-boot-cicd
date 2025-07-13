5a6e17672740f90034d52227d5a6ae5552b3be86
sokpanharith <sokpanharith.dev@gmail.com>
2025-07-13 23:56:57 +0700

    done

--- src/main/java/com/cicd/beginner/consumer/UsersListenerConfig.java
+++ src/main/java/com/cicd/beginner/consumer/UsersListenerConfig.java
@@ -10,34 +10,35 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @Slf4j
 public class UsersListenerConfig {
-    private final ObjectMapper objectMapper = new ObjectMapper();
+  private final ObjectMapper objectMapper = new ObjectMapper();
 
-    @Bean
-    public java.util.function.Consumer<ChangeEvent<String, String>> usersListener() {
-        return (event) -> {
-            try {
-                log.debug("Received CDC event from RabbitMQ - Key: {}, Value: {}", event.key(), event.value());
-                JsonNode valueNode = objectMapper.readTree(event.value());
-                String operation = valueNode.path("payload").path("op").asText();
-                JsonNode after = valueNode.path("payload").path("after");
-                JsonNode before = valueNode.path("payload").path("before");
+  @Bean
+  public java.util.function.Consumer<ChangeEvent<String, String>> usersListener() {
+    return (event) -> {
+      try {
+        log.debug(
+            "Received CDC event from RabbitMQ - Key: {}, Value: {}", event.key(), event.value());
+        JsonNode valueNode = objectMapper.readTree(event.value());
+        String operation = valueNode.path("payload").path("op").asText();
+        JsonNode after = valueNode.path("payload").path("after");
+        JsonNode before = valueNode.path("payload").path("before");
 
-                switch (operation) {
-                    case "c":
-                        log.info("Create Operation: {}", after);
-                        break;
-                    case "u":
-                        log.info("Update Operation - Before: {}, After: {}", before, after);
-                        break;
-                    case "d":
-                        log.info("Delete Operation: {}", before);
-                        break;
-                    default:
-                        log.warn("Unknown operation: {}", operation);
-                }
-            } catch (Exception e) {
-                log.error("Error processing CDC event: {}", e.getMessage(), e);
-            }
-        };
-    }
+        switch (operation) {
+          case "c":
+            log.info("Create Operation: {}", after);
+            break;
+          case "u":
+            log.info("Update Operation - Before: {}, After: {}", before, after);
+            break;
+          case "d":
+            log.info("Delete Operation: {}", before);
+            break;
+          default:
+            log.warn("Unknown operation: {}", operation);
+        }
+      } catch (Exception e) {
+        log.error("Error processing CDC event: {}", e.getMessage(), e);
+      }
+    };
+  }
 }
